33fb048865a0e32130e4f00b07da40641cdb075a
Matt Kotzbauer <mkotzbauer52@gmail.com>
2025-01-15 19:45:54 -0500

    Updated build.bat

--- driver/build.bat
+++ driver/build.bat
@@ -1,13 +1,9 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
 cl -FC -Zi /std:c++17 ..\driver\driver.cpp ..\driver\glad.c ^
 /I"..\include" ^
 /link /LIBPATH:"..\lib" ^
-/NODEFAULTLIB:LIBCMT ^
-/NODEFAULTLIB:MSVCRTD ^
 user32.lib Gdi32.lib opengl32.lib glfw3.lib ^
-shell32.lib msvcrt.lib vcruntime.lib ucrt.lib ^
 Ole32.lib Mmdevapi.lib
 popd
